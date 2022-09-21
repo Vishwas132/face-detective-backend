993655411e452e84234fad4e3018b12b05724b9c
Vishwas Lekhwar <64309462+Vishwas132@users.noreply.github.com>
2022-09-21 21:00:25 +0530

    Create an endpoint for route "/register"
    This method will take name, email and password and store it in an
    array as an object along with a random id.
    To generate random id nodejs in-build module "crypto" is used.
    For validation of user email and password "express validator" is used.
    It will respond to user request with the registered user's profile data
    that was stored in the array.

--- server.js
+++ server.js
@@ -1,13 +1,44 @@
 import express from "express";
+import { randomUUID } from "crypto";
+import { body, validationResult } from "express-validator";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const PORT = 3001;
+
+let users = [];
 
 // Homepage
 app.get("/", (req, res) => {
   res.status(200).json("success");
 });
+
+// Register a user
+app.post(
+  "/register",
+  body("email").isEmail(),
+  body("password").isLength({ min: 5 }),
+  (req, res) => {
+    const newUser = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    users.push({
+      id: randomUUID(),
+      name: newUser.name,
+      email: newUser.email,
+      password: newUser.password,
+      usageCounter: 0,
+      joined: new Date(),
+    });
+    res.status(200).json(users[users.length - 1]);
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Server listenig on http://localhost:${PORT}`);
